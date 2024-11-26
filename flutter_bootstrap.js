a88b537a8ac393b89c6ac25fa16b4272b5716e26
Ygor <ygor.felipe42@gmail.com>
2024-11-26 10:34:16 -0300

    impl - adicionado bordas

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "760929607"
+    serviceWorkerVersion: "663315206"
   }
 });
