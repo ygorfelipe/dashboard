23e7798ea4ce3c89884e920910310e1c2f706cfb
Ygor <ygor.felipe42@gmail.com>
2024-11-26 09:35:39 -0300

    impl - diversas

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "587661627"
+    serviceWorkerVersion: "3072409576"
   }
 });
